c7163da3e31cd8e094a81fd69eb493342044f764
alexia <alexia.dominguez@cuidum.com>
2024-10-21 18:00:08 +0200

    v8

--- java de dalto/codigo1.js
+++ java de dalto/codigo1.js
@@ -1,1 +1,15 @@
-"use strict"
+"strict mode";
+const validarEdad = (msg)=>{
+    let edad;
+    try{
+    if(msg) edad = prompt(msg);
+    else edad = prompt("introduce tu edad");
+    edad = parseInt(edad);
+    if(isNaN(edad)) throw "introduce tu edad";
+    if(edad>18) console.log("mayor de edad");
+    else console.log("menor");
+}catch(e){
+    validarEdad(e)
+}
+}
+validarEdad()
---
+++ java de dalto/codigo2.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const fecha = new Date();
+console.log(fecha.getYear()+1900)
+console.log(fecha.getMonth()+1)
+console.log(fecha.getDay())
