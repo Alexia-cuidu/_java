229cfbfc6ef5317fb0fc60ff9f5334088a5eb767
alexia <alexia.dominguez@cuidum.com>
2024-10-22 11:16:45 +0200

    v9

--- java de dalto/codigo2.js
+++ java de dalto/codigo2.js
@@ -1,6 +1,18 @@
 "use strict";
 
-const fecha = new Date();
-console.log(fecha.getYear()+1900)
-console.log(fecha.getMonth()+1)
-console.log(fecha.getDay())
+const addZeros = n => {
+    return n.toString().padStart(2, '0');
+}
+
+const actualizarHora = () => {
+    const time = new Date();
+    let hora = addZeros(time.getHours());
+    let min = addZeros(time.getMinutes());
+    let seg = addZeros(time.getSeconds()); 
+    document.querySelector(".hora").textContent = hora;
+    document.querySelector(".min").textContent = min;
+    document.querySelector(".seg").textContent = seg;
+}
+
+actualizarHora();
+setInterval(actualizarHora, 1000);
