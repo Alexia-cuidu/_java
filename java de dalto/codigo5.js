23deff1f8f7a6b0c9b75d8968df5ce3ec3d1b02e
alexia <alexia.dominguez@cuidum.com>
2024-10-23 12:28:50 +0200

    subo video con porcentaje y geocalizar a alguien x fotito

--- java de dalto/codigo5.js
+++ java de dalto/codigo5.js
@@ -3,20 +3,21 @@ const zona = document.querySelector(".zona-arrastre");
 
 zona.addEventListener("dragover", e => {
     e.preventDefault();
-    changeStyle(zona, "#444");  // Cambiado a 'zona'
+    changeStyle(zona, "#444");
 });
 
 zona.addEventListener("dragleave", e => {
     e.preventDefault();
-    changeStyle(zona, "#888");  // Cambiado a 'zona'
+    changeStyle(zona, "#888");
 });
 
 zona.addEventListener("drop", e => {
     e.preventDefault();
-    changeStyle(zona, "#888");  // Cambiado a 'zona'
+    changeStyle(zona, "#888");
     if (e.dataTransfer.files.length > 0) {
         cargarArchivo(e.dataTransfer.files[0]);
     }
+    zona.style.border = "4px solid #888";
 });
 
 const changeStyle = (obj, color) => {
@@ -26,10 +27,23 @@ const changeStyle = (obj, color) => {
 
 const cargarArchivo = ar => {
     const reader = new FileReader();
-    reader.readAsDataURL(ar);
+    
+    reader.readAsArrayBuffer(ar);
+    
+    reader.addEventListener("progress", e => {
+        if (e.lengthComputable) {
+            let carga = Math.round((e.loaded / ar.size) * 100);
+            console.log(`Cargando: ${carga}%`);
+            const barraCarga = document.querySelector(".barra-de-carga");
+            barraCarga.style.width = `${carga}%`;
+            barraCarga.querySelector(".porcentaje").textContent = `${carga}%`;
+        }
+    });
+
     reader.addEventListener("load", e => {
-        const url = e.target.result; 
-        let video = document.createElement("VIDEO");
+        const videoBlob = new Blob([new Uint8Array(e.target.result)], { type: 'video/mp4' });
+        const url = URL.createObjectURL(videoBlob);
+        const video = document.createElement("VIDEO");
         video.setAttribute("src", url);
         video.setAttribute("controls", "");
         document.querySelector(".resultado").appendChild(video);
